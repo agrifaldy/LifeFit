8cd480308613d8d9bf2343c2f67e5c55343b1859
agrifaldy <64409979+agrifaldy@users.noreply.github.com>
2020-12-01 22:41:27 +0700

    bisa

--- app/src/main/java/com/example/lifefit/MainActivity.java
+++ app/src/main/java/com/example/lifefit/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
         setContentView(R.layout.activity_main);
 
         // kita set default nya Home Fragment
@@ -79,7 +79,7 @@ public class MainActivity extends AppCompatActivity implements BottomNavigationV
     public void onBackPressed() {
 
         if (pressedTime + 2000 > System.currentTimeMillis()) {
-
+            //super.onBackPressed();
             android.os.Process.killProcess(android.os.Process.myPid());
             finish();
 
