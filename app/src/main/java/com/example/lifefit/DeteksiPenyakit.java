dabbcf5ed64b48a95745eaa65a43e1c71f492181
agrifaldy <64409979+agrifaldy@users.noreply.github.com>
2020-12-06 14:05:35 +0700

    deteksipenyakit

--- app/src/main/java/com/example/lifefit/DeteksiPenyakit.java
+++ app/src/main/java/com/example/lifefit/DeteksiPenyakit.java
@@ -11,18 +11,18 @@ import com.google.android.material.card.MaterialCardView;
 
 public class DeteksiPenyakit extends AppCompatActivity {
     private CardView cv_demam;
-    private Button b_demamIya;
-    private Button b_demamTidak;
+    private CardView b_demamIya;
+    private CardView b_demamTidak;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deteksi_penyakit);
 
-        b_demamIya = findViewById(R.id.b_demamIya);
+        /**b_demamIya = findViewById(R.id.cv_demamIya);
         if (b_demamIya.isClickable()) {
             b_demamIya.setBackgroundColor(Color.parseColor("#5ba19b"));
-        }
+        }**/
 
 
     }
