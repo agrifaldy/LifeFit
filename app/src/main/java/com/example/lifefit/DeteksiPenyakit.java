f6c1da812cb9c421ee1f1a5ae87a4e8ade2ee3f0
agrifaldy <64409979+agrifaldy@users.noreply.github.com>
2020-12-08 18:01:22 +0700

    deteksiPenyakit

--- app/src/main/java/com/example/lifefit/DeteksiPenyakit.java
+++ app/src/main/java/com/example/lifefit/DeteksiPenyakit.java
@@ -3,27 +3,51 @@ package com.example.lifefit;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 
 import com.google.android.material.card.MaterialCardView;
 
-public class DeteksiPenyakit extends AppCompatActivity {
+public class DeteksiPenyakit extends AppCompatActivity implements View.OnClickListener {
     private CardView cv_demam;
-    private CardView b_demamIya;
-    private CardView b_demamTidak;
+    private RadioButton demamIya;
+    private RadioButton demamTidak;
+    private RadioButton batukIya;
+    private RadioButton batukTidak;
+    private CardView submitDeteksi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_deteksi_penyakit);
 
-        /**b_demamIya = findViewById(R.id.cv_demamIya);
-        if (b_demamIya.isClickable()) {
-            b_demamIya.setBackgroundColor(Color.parseColor("#5ba19b"));
-        }**/
-
+        demamIya = (RadioButton) findViewById(R.id.demamIya);
+        demamIya.setOnClickListener(this);
+        demamTidak = (RadioButton) findViewById(R.id.demamTidak);
+        demamTidak.setOnClickListener(this);
+        batukIya = (RadioButton) findViewById(R.id.batukIya);
+        batukIya.setOnClickListener(this);
+        batukTidak = (RadioButton) findViewById(R.id.batukTidak);
+        batukTidak.setOnClickListener(this);
+        submitDeteksi = (CardView) findViewById(R.id.submitDeteksi);
+        submitDeteksi.setOnClickListener(this);
 
     }
+
+    public void onClick(View v) {
+
+        int i = v.getId();
+
+        if (demamIya.isChecked() && batukIya.isChecked()){
+            if (i == R.id.submitDeteksi) {
+                Intent intent = new Intent(this, Flu.class);
+                startActivity(intent);
+            }
+        }
+    }
+
 }
