9ea1c85a6fa7f156d160056bc6e8eefb3e1d0537
agrifaldy <64409979+agrifaldy@users.noreply.github.com>
2020-12-09 11:58:55 +0700

    barchart belum make database

--- app/src/main/java/com/example/lifefit/IndeksMassaTubuh/indeks_massa_tubuh_grafik.java
+++ app/src/main/java/com/example/lifefit/IndeksMassaTubuh/indeks_massa_tubuh_grafik.java
@@ -1,7 +1,9 @@
 package com.example.lifefit.IndeksMassaTubuh;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.collection.ArrayMap;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
@@ -11,141 +13,61 @@ import com.example.lifefit.R;
 import com.example.lifefit.IndeksMassaTubuh.Bmi;
 import com.example.lifefit.IndeksMassaTubuh.BmiAdapter;
 import com.github.mikephil.charting.animation.Easing;
-import com.github.mikephil.charting.charts.PieChart;
-import com.github.mikephil.charting.data.Entry;
-import com.github.mikephil.charting.data.PieData;
-import com.github.mikephil.charting.data.PieDataSet;
-import com.github.mikephil.charting.data.PieEntry;
-import com.github.mikephil.charting.highlight.Highlight;
-import com.github.mikephil.charting.listener.OnChartValueSelectedListener;
+import com.github.mikephil.charting.charts.BarChart;
+import com.github.mikephil.charting.data.BarData;
+import com.github.mikephil.charting.data.BarDataSet;
+import com.github.mikephil.charting.data.BarEntry;
 import com.github.mikephil.charting.utils.ColorTemplate;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.GenericTypeIndicator;
 import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class indeks_massa_tubuh_grafik extends AppCompatActivity {
 
-    PieChart pieChart;
     private DatabaseReference mDatabase = FirebaseDatabase.getInstance().getReference("Bmi");
     private FirebaseAuth mAuth;
+    private List<Bmi> list;
+    BarChart barChart;
+
+    /**public indeks_massa_tubuh_grafik(List<Bmi> list) {
+        this.list = list;
+    }**/
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_indeks_massa_tubuh_grafik);
-        pieChart = findViewById(R.id.pieChart);
-        initPieChart();
-        showPieChart();
-        //readData();
+        showBartChart();
 
     }
 
-    private void showPieChart(){
+    /**@Override
+    public int getItemCount() {
+        return list.size();
+    }**/
 
-        ArrayList<PieEntry> pieEntries = new ArrayList<>();
-        String label = "type";
-
-        //initializing data
-        Map<String, Integer> typeAmountMap = new HashMap<>();
-        typeAmountMap.put("",200);
-        typeAmountMap.put("Snacks",230);
-        typeAmountMap.put("Clothes",100);
-        typeAmountMap.put("Stationary",500);
-        typeAmountMap.put("Phone",50);
-
-        //initializing colors for the entries
-        ArrayList<Integer> colors = new ArrayList<>();
-        colors.add(Color.parseColor("#304567"));
-        colors.add(Color.parseColor("#309967"));
-        colors.add(Color.parseColor("#476567"));
-        colors.add(Color.parseColor("#890567"));
-        colors.add(Color.parseColor("#a35567"));
-        colors.add(Color.parseColor("#ff5f67"));
-        colors.add(Color.parseColor("#3ca567"));
-
-        //input data and fit data into pie chart entry
-        for(String type: typeAmountMap.keySet()){
-            pieEntries.add(new PieEntry(typeAmountMap.get(type).floatValue(), type));
-        }
-
-        //collecting the entries with label name
-        PieDataSet pieDataSet = new PieDataSet(pieEntries,label);
-        //setting text size of the value
-        pieDataSet.setValueTextSize(12f);
-        //providing color list for coloring different entries
-        pieDataSet.setColors(colors);
-        //grouping the data set from entry to chart
-        PieData pieData = new PieData(pieDataSet);
-        //showing the value of the entries, default true if not set
-        pieData.setDrawValues(true);
-
-        pieChart.setData(pieData);
-        pieChart.invalidate();
-    }
-
-    private void initPieChart(){
-        //using percentage as values instead of amount
-        pieChart.setUsePercentValues(true);
-
-        //remove the description label on the lower left corner, default true if not set
-        pieChart.getDescription().setEnabled(true);
-
-        //enabling the user to rotate the chart, default true
-        pieChart.setRotationEnabled(true);
-        //adding friction when rotating the pie chart
-        pieChart.setDragDecelerationFrictionCoef(0.9f);
-        //setting the first entry start from right hand side, default starting from top
-        pieChart.setRotationAngle(0);
-
-        //highlight the entry when it is tapped, default true if not set
-        pieChart.setHighlightPerTapEnabled(false);
-        //adding animation so the entries pop up from 0 degree
-        pieChart.animateY(1400, Easing.EaseInOutQuad);
-        //setting the color of the hole in the middle, default white
-        pieChart.setHoleColor(Color.parseColor("#ffffff"));
-
-        pieChart.setOnChartValueSelectedListener(new pieChartOnChartValueSelectedListener());
-
-    }
-
-    private class pieChartOnChartValueSelectedListener implements OnChartValueSelectedListener {
-
-        @Override
-        public void onValueSelected(Entry e, Highlight h) {
-            //trigger activities when entry is clicked
-        }
-
-        @Override
-        public void onNothingSelected() {
-
-        }
-    }
-
-    /**private  void readData(){
-        // Read from the database
+    private void showBartChart () {
 
         mDatabase.addValueEventListener(new ValueEventListener() {
             @Override
             public void onDataChange(DataSnapshot dataSnapshot) {
-                if (mAuth.getCurrentUser().getUid().equals(mDatabase.child("id").child("mAuth.getCurrentUser().getUid()"))) {
-                    // This method is called once with the initial value and again
-                    // whenever data at this location is updated.
-                    for (DataSnapshot snapshot : dataSnapshot.getChildren()){
-                        Bmi value = snapshot.getValue(Bmi.class);
-
-                    }
-
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                //if (mAuth.getCurrentUser().getUid().equals(value.getId())) {
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()){
+                    //Bmi value = snapshot.getValue(Bmi.class);
                 }
             }
-
 
             @Override
             public void onCancelled(DatabaseError error) {
@@ -153,6 +75,36 @@ public class indeks_massa_tubuh_grafik extends AppCompatActivity {
                 Log.w("TAG", "Failed to read value.", error.toException());
             }
         });
-    }**/
 
+        BarChart barChart = (BarChart) findViewById(R.id.barChart);
+
+        ArrayList<BarEntry> entries = new ArrayList<>();
+        entries.add(new BarEntry(2014, 420));
+        entries.add(new BarEntry(2015, 475));
+        entries.add(new BarEntry(2016, 508));
+        entries.add(new BarEntry(2017, 660));
+        entries.add(new BarEntry(2018, 550));
+        entries.add(new BarEntry(2019, 563));
+
+        BarDataSet bardataset = new BarDataSet(entries, "Tinggi");
+
+        /**ArrayList<String> labels = new ArrayList<String>();
+        labels.add("2016");
+        labels.add("2015");
+        labels.add("2014");
+        labels.add("2013");
+        labels.add("2012");
+        labels.add("2011");**/
+
+        bardataset.setColors(ColorTemplate.COLORFUL_COLORS);
+        bardataset.setValueTextColor(Color.BLACK);
+        bardataset.setValueTextSize(16f);
+        BarData barData = new BarData(bardataset);
+        barChart.setFitBars(true);
+        barChart.setData(barData); // set the data and list of labels into chart
+        barChart.getDescription().setText("Bar chart");
+
+
+        barChart.animateY(2000, Easing.EaseInOutQuad);
+    }
 }
