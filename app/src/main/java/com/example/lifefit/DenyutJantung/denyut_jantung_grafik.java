3bf13a691ebbbcde38a2a3da90a58ebb4b060517
Savvio <ahmadraihan015@gmail.com>
2020-12-19 19:54:46 +0700

    ppp

--- app/src/main/java/com/example/lifefit/DenyutJantung/denyut_jantung_grafik.java
+++ app/src/main/java/com/example/lifefit/DenyutJantung/denyut_jantung_grafik.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.WindowManager;
 
 import com.example.lifefit.R;
 import com.example.lifefit.TekananDarah.TekananDarah;
@@ -41,6 +42,7 @@ public class denyut_jantung_grafik extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_denyut_jantung_grafik);
         showBartChart();
     }
